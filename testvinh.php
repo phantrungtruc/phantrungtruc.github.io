705707dbba096a642f1590e67bd484ef4ced0bbb
Phan Trung Trực ♪ <phantrungtrucofficial.info@gmail.com>
2022-09-22 21:28:03 +0700

    upload

---
+++ testvinh.php
@@ -0,0 +1,8 @@
+<script language="javascript">
+var t = prompt("Nhập tên của bạn để mình biết ông nào mà khôn đến thế", '');
+alert(t);
+alert('RẤT NGU');
+</script>
+<body>
+    <a href="https://www.google.com/search?q=S%E1%BA%BD+G%E1%BA%A7y&rlz=1C1QCDH_viVN971VN971&oq=S%E1%BA%BD+G%E1%BA%A7y&aqs=chrome..69i57j0i131i433i512j0i512l4j0i131i433i512j0i512l2.11940j0j7&sourceid=chrome&ie=UTF-8"><input type="button" onclick="alert('CÓ LÀM MỚI CÓ ĂN! HÃY NGHE LỜI VĨNH. TỰ HỌC')" value="Click Me"/></a>
+  </body>
